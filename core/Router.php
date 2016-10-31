7fddf04b150d5a28ecf8f6c3bfc86bd93ac9c4db
DayLemon <lj3lj3@gmail.com>
2016-10-31 11:26:18 +0800

    修改路由匹配算法

--- core/Router.php
+++ core/Router.php
@@ -41,32 +41,70 @@ class Router
     }
 
     /**
-     * URI自动匹配算法：
-     * /admin/edit => AdminController@edit
+     * URI自动匹配算法：<br>
+     * 失效 -- /admin/edit => AdminController#edit <br>
+     * 失效 -- /admin/admin/edit => /admin/AdminController#edit <br>
+     * <br>
+     * 多个IndexController需要namespace支持 php5.3 <br>
+     * / => IndexController#index <br>
+     * /admin/ => /admin/IndexController#index <br>
+     * /admin/edit => /admin/IndexController#edit <br>
+     * /admin/post/edit => /admin/PostController#edit <br>
+     * /admin/author/post/edit => /admin/author/PostController#edit <br>
+     * <br>
+     * 在php<5.3的版本中将默认的IndexController换成与dirName相同的类名 <br>
+     * / => IndexController#index <br>
+     * /admin/ => /admin/AdminController#index <br>
+     * /admin/edit => /admin/AdminController#edit <br>
+     * /admin/post/edit => /admin/PostController#edit <br>
+     * /admin/author/post/edit => /admin/author/PostController#edit <br>
      */
     private function defaultDirect($uri)
     {
+        // url will be '' if user request root
         $uriPis = explode('/', $uri);
-        $functionName = $uriPis[count($uriPis)-1];
-        // This is for class
-        unset($uriPis[count($uriPis)-1]);
 
-//        $uriPisComplete = array_slice($uriPis, -1);
-        $className = $uriPis[count($uriPis)-1] = ucfirst($uriPis[count($uriPis)-1]);
+        $dirName = '';
+        $className = "Index";
+        $functionName = "index";
+
+        $pisCount = count($uriPis);
+        if ($pisCount == 1 && $uriPis[0] == '') {
+            // User request root directory
+            // Use default
+        } else if ($pisCount == 1 && $uriPis[0] != '') {
+            // User request 1-level directory
+            $dirName = $uriPis[0] . '/';
+            // 为兼容PHP<5.3
+            $className = ucfirst($uriPis[0]);
+        } else if ($pisCount == 2) {
+            // User request 2-level directory, use the second dir name as method name
+            $dirName = $uriPis[0] . '/';
+            // 为兼容PHP<5.3
+            $className = ucfirst($uriPis[0]);
+            $functionName = $uriPis[1];
+        } else {
+            // More than 2-level directory
+            $functionName = $uriPis[$pisCount-1];
+            $className = ucfirst($uriPis[$pisCount-2]);
+            // Cut the last two and glue it together
+            $dirName = implode('/', array_slice($uriPis, 0, $pisCount-2)) . '/';
+        }
+
         // use namespace
-        $path = implode('\\', $uriPis);
+//        $path = implode('\\', $uriPis);
 //        $realClassName =  "\\Core\\Controller\\{$path}Controller";
         $realClassName =  "{$className}Controller";
-        $controllerUri = 'Controller/' . implode('/', $uriPis) . 'Controller.php';
-        // last parameter is function
-//        $functionName = $uriPis[count($uriPis)-1];
+        $controllerUri = 'Controller/' . $dirName . $realClassName. '.php';
+        // Check the file exists or not
+        if (!file_exists(dirname(__FILE__) . "/" . $controllerUri)) {
+            throw new Exception(dirname(__FILE__) . "/" . $controllerUri . " does not exists!");
+        }
 
         require $controllerUri;
         // call this method
         $controller = new $realClassName;
 //        $controller->$functionName();
         call_user_func(array($controller, $functionName));
-
-//        $trimedUri = explode("/", $uri);
     }
 }
